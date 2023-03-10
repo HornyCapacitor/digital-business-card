ec5b2da6bb94de950be224b23067fa91bec949ef
Mateusz Minder <mateusz.minder99@gmail.com>
2023-03-10 20:35:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="card">
+      <img src="./public/profile-picture.jpg" alt="sailor dog with pipe"></img>
+    </div>
+  )
+}
+
+export default App
